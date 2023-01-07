d0645a080acf4b23ca279fb535afc033aefdb3e4
kirjavascript <snkenjoi@gmail.com>
2023-01-07 21:13:34 +0000

    button conversion, trying to load it

--- acebot.ino
+++ acebot.ino
@@ -7,14 +7,6 @@
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-const int buttonPin = 10;          // input pin for pushbutton
-int previousButtonState = HIGH;   // for checking the state of a pushButton
-int counter = 0;                  // button push counter
-int counter2 = 0;
-int frames = 0;
-
 #define RIGHT 11
 #define LEFT 1
 #define DOWN 8
@@ -26,27 +18,32 @@ int frames = 0;
 
 #define LATCH 0
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+int frames = 0;
+int frameCount = 0;
+
+char movie[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,64,64,0,0,0,0,64,64,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,16,0,16,0,16,64,16,64,16,64,0,64,16,64,0,64,16,0,16,64,0,64,0,64,0,64,8,0,0,0,0,0,0,0,0,0,0,0,0,16,0,16,0,0,0,0,0,0,1,9,0,0,0,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,64,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,32,0,32,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,34,0,32,0,32,0,32,0,32,0,32,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,32,0,32,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,34,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,16,0,18,0,16,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,34,0,34,0,34,0,34,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,32,16,32,16,32,0,32,0,32,0,0,0,0,0,0,0,0,16,0,16,0,16,0,16,0,16,0,18,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,16,32,16,32,16,32,16,32,0,32,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,34,0,34,0,2,0,2,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,18,0,16,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,16,34,0,32,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,18,0,16,0,16,0,16,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,17,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,16,0,16,0,16,1,0,1,0,33,32,0,32,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
 void setup() {
-  pinMode(buttonPin, INPUT);
+    Serial.begin(9600);
 
-  Serial.begin(9600);
-
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println(F("SSD1306 allocation failed"));
+        for(;;); // Don't proceed, loop forever
+    }
     display.clearDisplay();
 
-  display.setTextColor(WHITE);
+    display.setTextColor(WHITE);
 
-  display.setTextSize(3);
-  display.setCursor(0, 10);
-  display.println("acebot");
-  display.setTextSize(2);
-  display.setCursor(50, 35);
-  // Display static text
-  display.println("v0.1");
-  display.display();
+    display.setTextSize(3);
+    display.setCursor(0, 10);
+    display.println("acebot");
+    display.setTextSize(2);
+    display.setCursor(50, 35);
+    // Display static text
+    display.println("v0.1");
+    display.display();
 
     pinMode(RIGHT, OUTPUT);
     pinMode(LEFT, OUTPUT);
@@ -56,7 +53,7 @@ void setup() {
     pinMode(START, OUTPUT);
     pinMode(A, OUTPUT);
     pinMode(B, OUTPUT);
-
+    
     attachInterrupt(digitalPinToInterrupt(LATCH),latch_pulse, FALLING);
     digitalWrite(LEFT, 1);
     digitalWrite(RIGHT, 1);
@@ -66,18 +63,19 @@ void setup() {
     digitalWrite(SELECT, 1);
     digitalWrite(A, 1);
     digitalWrite(B, 1);
+
 }
 
 void latch_pulse() {
-  frames++;
-  //digitalWrite(LEFT, frames % 2);
-  writeButtons(frames);
-}
-
-// Write the given button sequence to the shift register
-void writeButtons(byte buttons)
-{
-    digitalWrite(A,      buttons & 1);
+    frames++;
+    //digitalWrite(DOWN, frames & 0b10);
+    if (frames & 0b10) {
+        
+      char buttons = movie[frameCount++ % 100];
+          
+      digitalWrite(A, buttons & 1); // this is the line that seems to 'freeze' it
+    /*
+    digitalWrite(A,buttons & 1);
     buttons = buttons >> 1;
     digitalWrite(B,      buttons & 1);
     buttons = buttons >> 1;
@@ -85,42 +83,22 @@ void writeButtons(byte buttons)
     buttons = buttons >> 1;
     digitalWrite(START,      buttons & 1);
     buttons = buttons >> 1;
-    digitalWrite(UP,      buttons & 1);
-    buttons = buttons >> 1;
-    digitalWrite(DOWN,      buttons & 1);
+    digitalWrite(RIGHT,      buttons & 1);
     buttons = buttons >> 1;
     digitalWrite(LEFT,      buttons & 1);
     buttons = buttons >> 1;
-    digitalWrite(RIGHT,      buttons & 1);
+    digitalWrite(DOWN,      buttons & 1);
+    buttons = buttons >> 1;
+    digitalWrite(UP,      buttons & 1);
+    */
+    }
 }
 
 
-
 void loop() {
-  /*int buttonState = digitalRead(buttonPin);
-
-  if ((buttonState != previousButtonState)
-      && (buttonState == HIGH)) {
-    counter++;
-  }
-  // save the current button state for comparison next time:
-  previousButtonState = buttonState;*/
-
-/*
-  digitalWrite(LEFT, frames % 2);
-    digitalWrite(RIGHT, 1);
-    digitalWrite(UP, 1);
-    digitalWrite(DOWN, 1);
-    digitalWrite(START, 1);
-    digitalWrite(SELECT, 1);
-    digitalWrite(A, 1);
-    digitalWrite(B, 1);
-    */
-    //if (frames % 50 == 0) {
-    
-  display.fillRect(0, 50, 160, 20, 0);
-  display.setCursor(0, 50);
-  display.println(frames);
-  display.display();
-    //}
+    // TODO: stream movie over serial
+    display.fillRect(0, 50, 160, 20, 0);
+    display.setCursor(0, 50);
+    display.println(frames);
+    display.display();
 }
---
+++ convert.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const inputs = fs.readFileSync('./Input Log.txt', 'utf8');
+const _pad = 'UDLRSsBA';
+const bytes = [];
+inputs.split('\n').forEach(line => {
+    if (line.startsWith('|')) {
+        const input = line.match(/\|(.{8})\|/)[1];
+        const byteStr = [...input].map(d => d == '.' ? 0 : 1).join('');
+        const byte = parseInt(byteStr, 2);
+        bytes.push(byte);
+    }
+});
+
+console.log(JSON.stringify(bytes));
+console.log(bytes.length);
